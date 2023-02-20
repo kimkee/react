5135f5e3d21753423a0eb66e0bd746047e210e9f
KimKeeHyun <kimkee7@gmail.com>
2023-02-20 13:06:53 +0900

    리스트 작업 연습

--- src/components/movie/List.jsx
+++ src/components/movie/List.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
-import Data from 'https://api.themoviedb.org/3/discover/movie?api_key=f76021076e8162ea929bd2cea62c6646&language=ko&region=kr&page=1&sort_by=release_date.desc';
-
-console.log(Data);
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function List() {
+
+const [datas, setDatas] = useState(null);
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(null);
+
+const fetchDatas = async () => {
+    try {
+      // 요청이 시작 할 때에는 error 와 datas 를 초기화하고
+      setError(null);	
+      setDatas(null);
+      // loading 상태를 true 로 바꿉니다.
+      setLoading(true);
+      const response = await axios.get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=f76021076e8162ea929bd2cea62c6646&language=ko&region=kr&page=1&sort_by=release_date.desc&page=1'
+      );
+      setDatas(response.data); // 데이터는 response.data 안에 들어있습니다.
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchDatas();
+  }, []);
+
+  if (loading) return <div>로딩중..</div>; 
+  if (error) return <div>에러가 발생했습니다</div>;
+
+	// 아직 datas가 받아와 지지 않았을 때는 아무것도 표시되지 않도록 해줍니다.
+  if (!datas) return null;
+  console.log(datas.results);
   return (
-	<div>List</div>
+	<div className='movie-list'>
+		<ul className='list'>
+		{
+			datas.results.map((data) =>{
+				console.log(data.poster_path);
+				const img = data.poster_path ? data.poster_path : "/9DVtwkuxzCLGVMapioeJ4RflfyW.jpg";
+				return(
+					<li key={data.id}>
+						<div className="box">
+							<div className="cont">
+								<div className="pics"><img src={`https://image.tmdb.org/t/p/original/${img}`} alt="" className='img'/></div>
+								<div className="desc">
+									<div className="tits">{data.original_title}</div>
+									<div className="text">{data.overview}</div>
+								</div>
+							</div>
+						</div>
+					</li>
+				)
+			})
+		}
+		</ul>
+	</div>
   )
 }
