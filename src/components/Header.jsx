c40a36be04e218e9cb0eb69353fff74425234c01
KimKeeHyun <kimkee7@gmail.com>
2023-02-20 14:15:07 +0900

    리스트 작업 연습

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
 	  <header className="header">
       <div className="inr">
         <div className="ldt">
-          <h1 className="logo"> <a href="/" className="btlogo"><i className="fa-brands fa-vuejs"></i></a></h1>
+          <h1 className="logo"> <a href="./react" className="btlogo"><i className="fa-brands fa-vuejs"></i></a></h1>
         </div>
         <div className="rdt">
           <button type="button" className="bt gnb"><i className="fa-regular fa-bars"></i><b>메뉴</b></button>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function Nav() {
   return (
-	  <nav id="menubar" class="menubar">
-      <div class="inr">
-        <ul class="menu">
-          <li><router-link class="bt" to="/"><i class="fa-regular fa-house"></i><em>Home</em></router-link></li>
-          <li><router-link class="bt" to="/bbs"><i class="fa-regular fa-list"></i><em>Board</em></router-link></li>
-          <li><router-link class="bt" to="/photo"><i class="fa-regular fa-camera"></i><em>Photo</em></router-link></li>
-          <li><router-link class="bt" to="/chat"><i class="fa-regular fa-comments"></i><em>Chat</em></router-link></li>
+	  <nav id="menubar" className="menubar">
+      <div className="inr">
+        <ul className="menu">
+          <li><a href='./react' className="bt"><i className="fa-regular fa-house"></i><em>Home</em></a></li>
+          <li><a href='./react' className="bt"><i className="fa-regular fa-list"></i><em>Board</em></a></li>
+          <li><a href='./react' className="bt"><i className="fa-regular fa-camera"></i><em>Photo</em></a></li>
+          <li><a href='./react' className="bt"><i className="fa-regular fa-comments"></i><em>Chat</em></a></li>
           <li>
-            <router-link v-if="!!$store.state.userInfo.uid" class="bt" to="/signout"><i class="fa-regular fa-right-from-bracket"></i><em>Logout</em></router-link>
-            <router-link v-else class="bt" to="/signin"><i class="fa-regular fa-user"></i><em> Login</em></router-link>
+            <a href='./react' className="bt"><i className="fa-regular fa-right-from-bracket"></i><em>Logout</em></a>
+            <a href='./react' className="bt"><i className="fa-regular fa-user"></i><em> Login</em></a>
           </li>
         </ul>
       </div>
