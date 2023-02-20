08d3e40a885c772cb4f4053caf23888ad1e3cf26
KimKeeHyun <kimkee7@gmail.com>
2023-02-20 16:08:57 +0900

    movie list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Header.jsx';
 import Nav from './components/Nav.jsx';
 import List from './components/movie/List.jsx';
-import './App.css'
+
 function App() {
 
 
--- src/components/movie/List.jsx
+++ src/components/movie/List.jsx
@@ -4,25 +4,30 @@ import axios from 'axios';
 export default function List() {
 
 const [datas, setDatas] = useState(null);
+const [genr, setGenr] = useState(null);
 const [loading, setLoading] = useState(false);
 const [error, setError] = useState(null);
 
 const fetchDatas = async () => {
-    try {
-      // 요청이 시작 할 때에는 error 와 datas 를 초기화하고
-      setError(null);	
-      setDatas(null);
-      // loading 상태를 true 로 바꿉니다.
-      setLoading(true);
-      const response = await axios.get(
-        'https://api.themoviedb.org/3/movie/now_playing?api_key=f76021076e8162ea929bd2cea62c6646&language=ko&region=kr&page=1&sort_by=release_date.desc&page=1'
-      );
-      setDatas(response.data); // 데이터는 response.data 안에 들어있습니다.
-    } catch (e) {
-      setError(e);
-    }
-    setLoading(false);
-  };
+  try {
+  // 요청이 시작 할 때에는 error 와 datas 를 초기화하고
+  setError(null);	
+  setDatas(null);
+  // loading 상태를 true 로 바꿉니다.
+  setLoading(true);
+  const response = await axios.get(
+  'https://api.themoviedb.org/3/movie/now_playing?api_key=f76021076e8162ea929bd2cea62c6646&language=ko&region=kr&page=1&sort_by=release_date.desc&page=1'
+  );
+  const getGenr = await axios.get(
+  'https://api.themoviedb.org/3/genre/movie/list?api_key=f76021076e8162ea929bd2cea62c6646&language=ko&region=kr'
+  );
+  setDatas(response.data); // 데이터는 response.data 안에 들어있습니다.
+  setGenr(getGenr.data); // 데이터는 response.data 안에 들어있습니다.
+  } catch (e) {
+    setError(e);
+  }
+  setLoading(false);
+};
 
   useEffect(() => {
     fetchDatas();
@@ -33,6 +38,13 @@ const fetchDatas = async () => {
 
 	// 아직 datas가 받아와 지지 않았을 때는 아무것도 표시되지 않도록 해줍니다.
   if (!datas) return null;
+  console.log(datas);
+ let cate = {}; 
+ genr.genres.forEach( d=>{
+	cate[d.id] = d.name;
+  });
+  console.log(cate[16]);
+  console.log(genr);
   console.log(datas.results);
   return (
 	<div className='movie-list'>
@@ -53,13 +65,16 @@ const fetchDatas = async () => {
 							</div>
 							<div className="info">
 								<div className="dd">
-									<div className="user"><span className="txt">{data.genre_ids}</span></div>
+									<div className="user">
+                    <span className="txt">
+                      <i className="fa-regular fa-list"></i>
+											{data.genre_ids.map( item => <em key={item}> {cate[item]}</em> )}
+										</span>
+									</div>
 								</div>
 								<div className="dd">
 									<div className="hits">
-										
-										
-										<em><i className="fa-regular fa-heart"></i> <b>{data.vote_average}</b></em>
+                    <em><i className="fa-regular fa-heart"></i> <b>{data.vote_average}</b></em>
 									</div>
 									<div className="date"><i className="fa-regular fa-calendar-days"></i> <b>{data.release_date}</b></div>
 								</div>
