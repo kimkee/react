efff2a38467553a8e4b8b7ccb755945d62954ae1
KimKeeHyun <kimkee7@gmail.com>
2023-02-22 22:59:39 +0900

    빌드 셋

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,9 +6,9 @@ export default function Nav() {
 	  <nav id="menubar" className="menubar">
       <div className="inr">
         <ul className="menu">
-          <li><NavLink to="/" className="bt"><i className="fa-regular fa-house"></i><em>Home</em></NavLink></li>
-          <li><NavLink to="/movie" className="bt"><i className="fa-regular fa-clapperboard-play"></i><em>Movie</em></NavLink></li>
-          <li><NavLink to="/search" className="bt"><i className="fa-regular fa-search"></i><em>Search</em></NavLink></li>
+          <li><NavLink to={`${process.env.PUBLIC_URL}/`} className="bt"><i className="fa-regular fa-house"></i><em>Home</em></NavLink></li>
+          <li><NavLink to={`${process.env.PUBLIC_URL}/movie`} className="bt"><i className="fa-regular fa-clapperboard-play"></i><em>Movie</em></NavLink></li>
+          <li><NavLink to={`${process.env.PUBLIC_URL}/search`} className="bt"><i className="fa-regular fa-search"></i><em>Search</em></NavLink></li>
           {/* <li><NavLink to="/movie/list" className="bt"><i className="fa-regular fa-camera"></i><em>Search</em></NavLink></li> */}
           {/* <li><a href='./react' className="bt"><i className="fa-regular fa-comments"></i><em>Chat</em></a></li>
           <li>
