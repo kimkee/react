2c6b5c383a9202fb9eb79adb63ca3b5c90327038
KimKeeHyun <kimkee7@gmail.com>
2023-03-17 09:15:31 +0900

    VER

--- buildv.js
+++ buildv.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 
 const d = new Date();
-const ver = d.getFullYear()+""+(d.getMonth()+1) +""+ d.getDate() +""+ d.getHours() +""+ d.getMinutes() +""+ d.getSeconds();
+let yy = d.getFullYear();
+let mm = d.getMonth()+1;
+let dd = d.getDate();
+let hh = d.getHours();
+let min = d.getMinutes();
+let sec = d.getSeconds();
 
+const digt = n=> n < 10 ? "0"+n : n ;
+const ver = yy + digt(mm) + digt(dd) + digt(hh) + digt(min) + digt(sec);
+console.log(ver);
 // .env 파일에 변수를 설정합니다.
 fs.writeFileSync('.env.production', `REACT_APP_VER=V${ver}`);
