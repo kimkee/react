39daee7b5b11cc3fc25440c5ce66f0395e03456a
KimKeeHyun <kimkee7@gmail.com>
2023-03-24 12:48:16 +0900

    포스터 팝업 스와이퍼

--- src/pages/movie/Poster.jsx
+++ src/pages/movie/Poster.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Outlet,useParams, useNavigate} from 'react-router-dom'; //,useOutletContext  , useLocation
+import {useParams, useNavigate} from 'react-router-dom'; //,useOutletContext  , useLocation, Outlet,
 import ui from '../../ui';
 
 
--- src/pages/movie/View.jsx
+++ src/pages/movie/View.jsx
@@ -214,7 +214,7 @@ export default function View() {
                     datas.images.posters.map((img,idx) => {
                       return(
                       <div key={idx} className='box'>
-                        <div  className='pic'><img src={'https://image.tmdb.org/t/p/w300'+img.file_path} alt={img.name} className="img" onError={(e)=>{e.target.src=`${process.env.REACT_APP_PUBLIC_URL}img/common/non_poster.png`}} loading="lazy" /></div> 
+                        <Link to={`./poster`} className='pic'><img src={'https://image.tmdb.org/t/p/w300'+img.file_path} alt={img.name} className="img" onError={(e)=>{e.target.src=`${process.env.REACT_APP_PUBLIC_URL}img/common/non_poster.png`}} loading="lazy" /></Link> 
                       </div>
                       )
                     })
