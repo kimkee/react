dc19dcb3c33a89de3698bc0dca1d5aec403960e9
KimKeeHyun <kimkee7@gmail.com>
2023-03-27 18:15:28 +0900

    datas.runtime

--- src/pages/movie/View.jsx
+++ src/pages/movie/View.jsx
@@ -157,7 +157,7 @@ export default function View({prop}) {
                         <i className=" fa-regular fa-camera-movie"></i>  {datas.first_air_date} ~ {datas.last_air_date}
                       </li>}
                       
-                      {/* datas.runtime &&  */
+                      {datas.runtime && 
                       <li className="tim">
                         <i className="fa-regular fa-timer"></i> <b>시간</b> : {datas.runtime} 분
                       </li>} 
