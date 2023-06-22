3959504d5f905dc204351ab0a1db076b7f8fe283
KimKeeHyun <kimkee7@gmail.com>
2023-06-22 22:18:44 +0900

    카테고리 메뉴 작업1

--- src/components/CateMenu.jsx
+++ src/components/CateMenu.jsx
@@ -13,62 +13,53 @@ import 'swiper/css/scrollbar';
 export default function CateMenu({menu, opts}) {
   let params = useParams()
   // let navigate = useNavigate();
-  const [swiper, setSwiper] = useState(null);
+  // const [swiper, setSwiper] = useState(null);
   const [slideActive, slideActiveSet] = useState(0);
   const goSlide = (num) => { 
     document.querySelectorAll(".cate-box ul>li .bt.active").forEach( bt => {
       num = parseInt( bt.closest("li").getAttribute("data-index") );
-    })
+    });
+    const catebox = document.querySelector(".cate-box>.inr");
+    const cateboxWid = catebox.offsetWidth * 0.5;
+    const btnAct = document.querySelector(".cate-box ul>li .bt.active");
+    const btnActLeft = btnAct?.offsetLeft;
+    const btnActWid = btnAct?.offsetWidth * 0.5;
+
+    const scr = btnActLeft - cateboxWid + btnActWid;
+    catebox.scrollTo( scr , 0)
+
     slideActiveSet(  num  )
-    swiper?.slideTo( slideActive , 100 );
+    // swiper?.slideTo( slideActive , 100 );
+    console.log("slideActive == " + slideActive + "  " + btnActLeft);
   };
   const cateID = params.cate;
-  console.log(slideActive);
   useEffect(() => {
     goSlide(slideActive)
     return ()=>{
     }
     // eslint-disable-next-line
-  },[swiper,cateID,slideActive,menu,opts]);
+  },[cateID,slideActive,menu,opts]);
 
   return (
   <>
     <div className="cate-box">
       <div className="inr">
         {/* <button onClick={goSlide}>SSSSS</button> */}
-        <Swiper className="swiper-wrapper" 
-          // install Swiper modules
-          modules={[Navigation, Pagination,FreeMode, A11y]}
-          spaceBetween={0}
-          slidesPerView={'auto'}
-          // slidesPerGroup={5}
-          slidesPerGroupAuto={true}
-          freeMode={true}
-          // navigation
-          loop={false }
-          wrapperTag="ul" 
-          // pagination={{ clickable: true }}
-          // scrollbar={{ draggable: true }}
-          // initialSlide={ Math.floor( Math.random() *10  ) } // 0 ~ 9
-          autoHeight={false}
-          onSwiper={(swiper) => {
-            console.log("initialize swiper", swiper);
-            setSwiper(swiper);
-          }}
-        >
-
-            <SwiperSlide tag="li" data-index="0" className="swiper-slide pbox">
+        
+          <ul className="list">
+            <li data-index="0" className="pbox">
               <NavLink type="button" className={ "bt" } to={`/list/${opts}/0`}>전체</NavLink>
-            </SwiperSlide>
+            </li>
           { menu.map( (item,idx) => {
             return (
-            <SwiperSlide tag="li" data-index={idx+1} key={item.id}  className="swiper-slide pbox">
+            <li data-index={idx+1} key={item.id}  className="pbox">
               <NavLink type="button" className={ item.id === cateID ? "bt active" : "bt " } to={`/list/${opts}/${item.id}`}> { [item.name]  }</NavLink>
-            </SwiperSlide>
+            </li>
             )
           })}
+          </ul>
               
-        </Swiper>
+        
       </div>
     </div>
   </>  
