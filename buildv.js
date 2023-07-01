78d1f857aaf4eeac3c0b277e5e38b7dfc2b71e7b
김기현 <kimkee7@gmail.com>
2023-07-01 12:59:48 +0000

    bb

--- buildv.js
+++ buildv.js
@@ -12,4 +12,4 @@ const digt = n => n < 10 ? "0"+n : n ;
 const ver = yy + digt(mm) + digt(dd) + digt(hh) + digt(min) + digt(sec);
 console.log(ver);
 // .env 파일에 변수를 설정합니다.
-fs.writeFileSync('.env.production', `VITE_APP_VER=${ver}`);
+fs.writeFileSync('.env.production', `VITE_APP_VER='${ver}'`);
