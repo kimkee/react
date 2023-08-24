53a8df2f4b0ffd280cf06a667d25e54322a72fdd
KimKeeHyun <kimkee7@gmail.com>
2023-08-24 16:25:42 +0900

    회원가입 유효성 첵크

--- src/pages/user/SignUp.jsx
+++ src/pages/user/SignUp.jsx
@@ -28,41 +28,32 @@ export default function SignUp() {
     "auth/internal-error": "비밀번호를 입력하세요.",
   }
 
-  const userEmail = useRef(null);
-  const userPassword = useRef();
-  const userNick = useRef();
-  const avatarVal = useRef();
-
   const join = async ()=>{
     
   }
-  
-  const [isEmailValid, setIsEmailValid] = useState(false);
-  const [isPwdValid, setIsPwdValid] = useState(false);
-  const [isNickValid, setIsNickValid] = useState(false);
 
-  const [inputState, setInputState] = useState({
-    isEmailValid: false,
-    isPwdValid: false,
-    isNickValid: false,
+  const [inpVal, setInpVal] = useState({
+    isMail: false,
+    isPwds: false,
+    isNick: false,
   });
-  console.log(inputState);
+  console.log(inpVal);
   const validate = (input) => {
     const pattern = {
       mail : /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,  // 이메일 형식
-      pass : /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/, // 문자,숫자 포함6자리 이상
+      pass : /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,15}$/, // 문자,숫자 포함6자리 이상 15자 이하
       nick : /^[A-Za-z0-9]{1,15}$/ // 특수문자 제외하고 , 문자 또는 숫자 1자리 이상 15자 이하
     }
     switch (input.name) {
-      case 'email':    setInputState( inputState => ({ ...inputState, isEmailValid: pattern.mail.test(input.value), })); break;
-      case 'password': setInputState( inputState => ({ ...inputState, isPwdValid: pattern.pass.test(input.value), })); break;
-      case 'nickname': setInputState( inputState => ({ ...inputState, isNickValid: pattern.nick.test(input.value), })); break;
+      case 'email':    setInpVal( inpVal => ({ ...inpVal, isMail: pattern.mail.test(input.value), })); break;
+      case 'password': setInpVal( inpVal => ({ ...inpVal, isPwds: pattern.pass.test(input.value), })); break;
+      case 'nickname': setInpVal( inpVal => ({ ...inpVal, isNick: pattern.nick.test(input.value), })); break;
       // case 'nickname': setIsNickValid( pattern.nick.test(input.value) ); break;
       default: break;
     }
   };
 
-  console.log( isEmailValid , isPwdValid , isNickValid , isEmailValid && isPwdValid && isNickValid );
+  console.log( inpVal.isMail , inpVal.isPwds , inpVal.isNick , inpVal.isMail && inpVal.isPwds && inpVal.isNick );
   useEffect( () => {
     window.scrollTo(0,0);
     // document.querySelector(".header").classList.remove("trans");
@@ -91,7 +82,7 @@ export default function SignUp() {
                   { store.state.avatar.map( (icon,index) => {
                       return (
                         <label key={index}>
-                          <input type="radio" name="avatar" value={index} ref={avatarVal} defaultChecked={index == 0 } />
+                          <input type="radio" name="avatar" value={index} defaultChecked={index == 0 } />
                           <span className="txt"><img className="img" src={icon} alt="" /></span>
                         </label>
                       )
@@ -102,27 +93,27 @@ export default function SignUp() {
             <li>
               <label className="dt">이메일</label>
               <div className="dd">
-                <span className="input"><input ref={userEmail} name="email" type="email" placeholder="예) test@naver.com" onInput={(e)=>validate(e.currentTarget)} data-webkit-autofill /></span>
-                {!inputState.isEmailValid ? <p className={`msg-valid`}>이메일 형식에 맞게 입력해주세요.</p> : <i className="chk fa-regular fa-check"></i>}
+                <span className="input"><input name="email" type="email" placeholder="예) test@naver.com" onInput={(e)=>validate(e.currentTarget)} data-webkit-autofill /></span>
+                {!inpVal.isMail ? <p className={`msg-valid`}>이메일 형식에 맞게 입력해주세요.</p> : <i className="chk fa-regular fa-check"></i>}
               </div>
             </li>
             <li>
               <label className="dt">비밀번호</label>
               <div className="dd">
-                <span className="input"><input ref={userPassword} name="password" type="password" placeholder="예) abc123" onInput={(e)=>validate(e.currentTarget)} /></span>
-                {!inputState.isPwdValid ? <p className={`msg-valid`}>문자,숫자 포함 6자리 이상 입력해주세요.</p> : <i className="chk fa-regular fa-check"></i>}
+                <span className="input"><input name="password" type="password" placeholder="예) abc123" onInput={(e)=>validate(e.currentTarget)} maxLength={15} /></span>
+                {!inpVal.isPwds ? <p className={`msg-valid`}>문자,숫자 포함 6자리 이상 입력해주세요.</p> : <i className="chk fa-regular fa-check"></i>}
               </div>
             </li>
             <li>
               <label className="dt">닉네임</label>
               <div className="dd">
-                <span className="input"><input ref={userNick} name="nickname" type="text" placeholder="입력하세요" onInput={(e)=>validate(e.currentTarget)} /></span>
-                {!inputState.isNickValid ? <p className={`msg-valid`}>문자,숫자 포함 6자리 이상 입력해주세요.</p> : <i className="chk fa-regular fa-check"></i>}
+                <span className="input"><input name="nickname" type="text" placeholder="입력하세요" onInput={(e)=>validate(e.currentTarget)} maxLength={15} /></span>
+                {!inpVal.isNick ? <p className={`msg-valid`}>특수문자 제외하고 입력해주세요.</p> : <i className="chk fa-regular fa-check"></i>}
               </div>
             </li>
           </ul>
           <div className="btsbox btn-set">
-            <button type="button" className="btn" disabled={!(inputState.isEmailValid && inputState.isPwdValid && inputState.isNickValid)} onClick={join}><i className="fa-regular fa-right-to-bracket"></i><em>회원가입</em></button>
+            <button type="button" className="btn" disabled={!(inpVal.isMail && inpVal.isPwds && inpVal.isNick)} onClick={join}><i className="fa-regular fa-right-to-bracket"></i><em>회원가입</em></button>
           </div>
           <div className="link">
             이미 회원이신가요? <Link className="bt" to="/user/signin">로그인 하러가기 <i className="fa-regular fa-chevron-right"></i></Link>
