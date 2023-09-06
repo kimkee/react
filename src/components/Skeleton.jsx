e2226f908a979f50098e481003e04be1787d3a27
KimKeeHyun <kimkee7@gmail.com>
2023-09-06 11:26:13 +0900

    스켈레톤 ui

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -1,34 +1,24 @@
 
 import React, { useState, useEffect } from 'react';
-import { Link  } from 'react-router-dom';  // useParams , Outlet, useSearchParams, useLocation
+import { Link } from 'react-router-dom';  // useParams , Outlet, useSearchParams, useLocation
 import axios from 'axios';
 
-export default  function Skeleton({opts}){
-  
-  const list = [];
-  // let i = 0;
-  // while ( i <= opts.num) {
-  //   list.push(<li key={i}><div className="box"><div className="cont"><div className="pics"></div></div><div className="tits"></div></div></li>);
-  //   i++;
-  // }
-  for (let i = 0; i <= opts.num ; i++) {
-    list.push(<li key={i}><div className="box"><div className="cont"><div className="pics"></div></div><div className="tits"></div></div></li>);
-    // opts.type == 'movie-list' ? list.push(<li key={i}><div className="box"><div className="cont"><div className="pics"></div></div><div className="tits"></div></div></li>) : null;
+export default function Skeleton({ opts }) {
+
+  const skeltList = [];
+
+  for (let i = 0; i <= opts.num; i++) {
+    opts.type == 'movie-list' && skeltList.push(
+      <li key={i}>
+        <div className="box">
+          <div className="cont">
+            <div className="pics"></div>
+          </div>
+          <div className="tits"></div>
+        </div>
+      </li>
+    );
   }
 
-  useEffect(() => {
-    console.log(list);
-    return ()=>{
-      
-    }
-    // eslint-disable-next-line
-  },[]);
-
-
-
-  return(
-    <> 
-      {list}
-    </>
-  )
+  return (skeltList)
 }
