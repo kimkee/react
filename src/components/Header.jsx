f619ac97d60c178a304b88ed361d4010db1a1971
KimKeeHyun <kimkee7@gmail.com>
2023-09-11 15:09:24 +0900

    헤더 유저정보

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React, {useState,  useEffect } from 'react'; //
 import {Link, NavLink, useParams, useLocation, useNavigate} from 'react-router-dom'; // ,useParams,useLocation
 import ui from '/src/ui.js';
 import store from '../store.js';
-import isuser from '../getUser.js';
+import getUserData from '../getUser.js';
 import { getAuth, onAuthStateChanged, signOut, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 export default function Header({prop}) {
   let params = useParams();
@@ -15,7 +15,10 @@ export default function Header({prop}) {
   const [userInfo, setUserInfo] = useState({});
   
   useEffect(() => {
-    console.log(isuser);
+    console.log(getUserData() );
+    getUserData().then((userData) => {
+      console.log(userData); // 얻은 사용자 데이터를 사용하세요
+    });
     setUserInfo(sessionStorage.user && JSON.parse(sessionStorage.user))
     const auth = getAuth();
     onAuthStateChanged(auth, (authUser) => {
--- src/getUser.js
+++ src/getUser.js
@@ -1,23 +1,25 @@
-
+// getUserData.js
 import { db } from './firebaseConfig.js';
 import { getDoc, doc } from 'firebase/firestore';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
-let userData = {};
-if (sessionStorage?.user) {
-  const userID = JSON.parse(sessionStorage?.user).uid;
-  const docRef = doc(db, "member", userID);
-  try {
-    const docSnap = await getDoc(docRef);
+// async 함수로 래핑
+const getUserData = async () => {
+  let userData = {};
 
-    console.log(docSnap.data());
-    userData = docSnap.data()
-  } catch (error) {
-    console.log(error)
+  if (sessionStorage?.user) {
+    const userID = JSON.parse(sessionStorage?.user).uid;
+    const docRef = doc(db, "member", userID);
+    try {
+      const docSnap = await getDoc(docRef);
+
+      // 사용자 데이터 반환
+      userData = docSnap.data();
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-}
-const getData = async function () {
-}
-getData();
-export default userData;
+  return userData; // 사용자 데이터 반환
+};
+
+export default getUserData;
