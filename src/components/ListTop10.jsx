f1322f632e912cf657e624bd5d783de355374c77
KimKeeHyun <kimkee7@gmail.com>
2023-09-14 18:03:12 +0900

    정리

--- src/components/ListTop10.jsx
+++ src/components/ListTop10.jsx
@@ -3,15 +3,6 @@ import React, { useState, useEffect } from 'react';
 import { Link  } from 'react-router-dom';  // useParams , Outlet, useSearchParams, useLocation
 import axios from 'axios';
 
-// import Swiper core and required modules
-// import { Navigation, Pagination, Scrollbar, FreeMode, A11y } from 'swiper';
-// import { Swiper, SwiperSlide } from 'swiper/react'; //, useSwiper 
-
-// Import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-// import 'swiper/css/scrollbar';
 import ui from '../ui.js';
 import StarPoint from '../components/StarPoint';
 
@@ -36,30 +27,18 @@ export default  function ListSet({opts}){
 
     axios.get( fetchURL ).then(res =>{
       console.log(res.data);
-      setMlist( mlist => [...mlist,...res.data.results] );
+      setMlist( predata => [...predata, ...res.data.results] );
       // console.log( mlist );
-      console.log(page + "=== " + res.data.total_pages );
-      
-      
       ui.loading.hide();
       if( res.data.total_pages <= page ) {
-      
-        document.querySelector(".ui-loadmore")?.classList.add("hide");
       };
 
 
     }).catch(e=>{
       console.log(e);
       ui.loading.hide();
-      document.querySelector(".ui-loadmore")?.classList.add("error");
     }); 
   }
-
-  
-  // const [swiper, setSwiper] = useState(null);
-  // const nexto = () => {
-  //   swiper.slideTo( Math.floor( Math.random() *10 ) , 0 );
-  // };
 
   useEffect(() => {
     fetchMoive(1);
@@ -80,12 +59,16 @@ export default  function ListSet({opts}){
         <div className="inr">
           <div className="slide">
             <ul>
-              { mlist?.filter( (item, i) => i < 20 ).map( (data, idx) => {
-                const img = 'https://image.tmdb.org/t/p/w154'+data.poster_path ;
+              { mlist?.filter( (item, idx) => idx < 20 ).map( (data, idx) => {
+                const img = `//image.tmdb.org/t/p/w154${data.poster_path}`;
                 return (
                   <li key={idx}  className="pbox">
                     <Link className="box" to={`${opts.opts}/${data.id}`}>
-                      <div className="pics"><img src={`${img}`} alt="" className='img' onError={(e)=>{e.target.src=`${import.meta.env.VITE_APP_PUBLIC_URL}img/common/non_poster.png`}} /></div>
+                      <div className="pics">
+                        <img src={`${img}`} alt="" className='img' 
+                          onError={ e => e.target.src=`${import.meta.env.VITE_APP_PUBLIC_URL}img/common/non_poster.png` } 
+                        />
+                      </div>
                       <div className="info"><StarPoint point={data.vote_average} /></div>
                     </Link>
                   </li>
