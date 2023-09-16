315bef0dcdc2585c6f3a709638b8cc33768931e0
KimKeeHyun <kimkee7@gmail.com>
2023-09-16 19:58:58 +0900

    빌드

--- src/pages/movie/Detail.jsx
+++ src/pages/movie/Detail.jsx
@@ -15,7 +15,6 @@ export default function Detail({ postID, popTitle }) {
   const [casts, setCasts] = useState(null);
   const [moves, setMovs] = useState(null);
   const [bgImg, setBgImg] = useState('');
-  const [childState, setChildState] = useState('');
   const fetchURL = `https://api.themoviedb.org/3/${opts}/${postID}?language=ko&region=kr&api_key=${import.meta.env.VITE_TMDB_API_KEY}&append_to_response=images&include_image_language=en,null`;
   const fetchDatas = () => {
     axios.get( fetchURL ).then(response => {
