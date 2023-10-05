5863985f26a4c2a6170d4c2dca1e82e93ba3ec3f
KimKeeHyun <kimkee7@gmail.com>
2023-10-05 16:46:24 +0900

    스크랩 삭제 작업

--- src/pages/user/UserLike.jsx
+++ src/pages/user/UserLike.jsx
@@ -90,7 +90,7 @@ export default function UserLike({uInfo}) {
                   </li>
                 )
             }).reverse()}
-            
+            { uInfo.tmdb_movie_scrap.length % 2 === 1 && <li></li>}
           </ul>
           :
           <div className="nodata">
