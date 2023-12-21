be33b528a5d076ccc1a57b9154be82c2cd893b5f
KimKeeHyun <kimkee7@gmail.com>
2023-12-21 12:34:44 +0900

    VITE_APP_VER

--- buildv.js
+++ buildv.js
@@ -8,8 +8,8 @@ let hh = d.getHours();
 let min = d.getMinutes();
 let sec = d.getSeconds();
 
-const digt = n => n < 10 ? "0"+n : n ;
-const ver = `${ yy +'-'+ digt(mm) +'-'+ digt(dd) +'-'+ digt(hh) +'-'+ digt(min) +'-'+ digt(sec)}`;
+const digt = n => n < 10 ? ".0"+n : "."+n ;
+const ver = yy + digt(mm) + digt(dd) + digt(hh) + digt(min) + digt(sec);
 console.log(ver);
 // .env 파일에 변수를 설정합니다.
 fs.writeFileSync('.env.production', `VITE_APP_VER='${ver}'`);
