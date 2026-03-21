191e759f12653a2be3df062cb9fbc6919fd5b0be
KimKeeHyun <kimkee7@gmail.com>
2026-03-21 11:31:39 +0900

    Noto Sans KR NPM 설치

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '@fontsource/noto-sans-kr';
+import '@fontsource/noto-sans-kr/700.css'; // Add bold weight
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
